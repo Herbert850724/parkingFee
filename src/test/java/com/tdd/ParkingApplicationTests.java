8778c0b7fcdc4a0a03add110aea612cdf5e31761
herbe <herbert850724@gmail.com>
2024-08-06 11:28:40 +0800

    fee riles

--- src/test/java/com/tdd/ParkingApplicationTests.java
+++ src/test/java/com/tdd/ParkingApplicationTests.java
@@ -9,64 +9,39 @@ import java.time.LocalDateTime;
 @SpringBootTest
 class ParkingApplicationTests {
 
-    @Test
-    void freeFor15Min() {
+    private void runTest(String start,String end,Long expect){
         ParkingFeeCalculator pfc = new ParkingFeeCalculator();
 
         Long actual = pfc.calculate(
-                LocalDateTime.of(2024,1,1,0,0,0)
-                ,LocalDateTime.of(2024,1,1,0,14,0));
+                LocalDateTime.parse(start)
+                ,LocalDateTime.parse(end));
 
-        Assertions.assertThat(actual).isEqualTo(0L);
+        Assertions.assertThat(actual).isEqualTo(expect);
+    }
+
+    @Test
+    void freeFor15Min() {
+        runTest("2024-01-01T00:00:00","2024-01-01T00:14:00",0L);
     }
 
     @Test
     void feeFor15Min(){
-        ParkingFeeCalculator pfc = new ParkingFeeCalculator();
-
-        Long actual = pfc.calculate(
-                LocalDateTime.of(2024,1,1,0,0,0)
-                ,LocalDateTime.of(2024,1,1,0,15,0)
-        );
-
-        Assertions.assertThat(actual).isEqualTo(30L);
+        runTest("2024-01-01T00:00:00","2024-01-01T00:15:00",30L);
     }
 
     @Test
     void over30under60Fee(){
-
-        ParkingFeeCalculator pfc = new ParkingFeeCalculator();
-
-        Long actual = pfc.calculate(
-                LocalDateTime.of(2024,1,1,0,0,0)
-                ,LocalDateTime.of(2024,1,1,0,45,0)
-        );
-
-        Assertions.assertThat(actual).isEqualTo(60L);
+        runTest("2024-01-01T00:00:00","2024-01-01T00:45:00",60L);
     }
 
     @Test
     void over60under90Fee(){
-        ParkingFeeCalculator pfc = new ParkingFeeCalculator();
-
-        Long actual = pfc.calculate(
-                LocalDateTime.of(2024,1,1,0,0,0)
-                ,LocalDateTime.of(2024,1,1,1,0,0)
-        );
-
-        Assertions.assertThat(actual).isEqualTo(90L);
+        runTest("2024-01-01T00:00:00","2024-01-01T01:00:00",90L);
     }
 
     @Test
     void over150Pay150(){
-        ParkingFeeCalculator pfc = new ParkingFeeCalculator();
-
-        Long actual = pfc.calculate(
-                LocalDateTime.of(2024,1,1,0,0,0)
-                ,LocalDateTime.of(2024,1,1,9,0,0)
-        );
-
-        Assertions.assertThat(actual).isEqualTo(150L);
+        runTest("2024-01-01T00:00:00","2024-01-01T19:00:00",150L);
     }
 
 }
