a0996a26638fbb135c34cc14f85d9657eb8f0e4f
herbe <herbert850724@gmail.com>
2024-08-06 10:19:35 +0800

    parking fee

---
+++ src/main/java/com/tdd/ParkingFeeCalculator.java
@@ -0,0 +1,19 @@
+package com.tdd;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class ParkingFeeCalculator {
+
+    public Long calculate(LocalDateTime start,LocalDateTime end){
+
+        long minsBetween = ChronoUnit.MINUTES.between(start, end);
+        if(minsBetween<15){
+            return 0L;
+        }
+        if(minsBetween<30){
+            return 30L;
+        }
+        return 60L;
+    }
+}
---
+++ src/test/java/com/tdd/ParkingApplicationTests.java
@@ -0,0 +1,48 @@
+package com.tdd;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+class ParkingApplicationTests {
+
+    @Test
+    void freeFor15Min() {
+        ParkingFeeCalculator pfc = new ParkingFeeCalculator();
+
+        Long actual = pfc.calculate(
+                LocalDateTime.of(2024,1,1,0,0,0)
+                ,LocalDateTime.of(2024,1,1,0,14,0));
+
+        Assertions.assertThat(actual).isEqualTo(0L);
+    }
+
+    @Test
+    void feeFor15Min(){
+        ParkingFeeCalculator pfc = new ParkingFeeCalculator();
+
+        Long actual = pfc.calculate(
+                LocalDateTime.of(2024,1,1,0,0,0)
+                ,LocalDateTime.of(2024,1,1,0,15,0)
+        );
+
+        Assertions.assertThat(actual).isEqualTo(30L);
+    }
+
+    @Test
+    void over30under60Fee(){
+
+        ParkingFeeCalculator pfc = new ParkingFeeCalculator();
+
+        Long actual = pfc.calculate(
+                LocalDateTime.of(2024,1,1,0,0,0)
+                ,LocalDateTime.of(2024,1,1,0,45,0)
+        );
+
+        Assertions.assertThat(actual).isEqualTo(60L);
+    }
+
+}
