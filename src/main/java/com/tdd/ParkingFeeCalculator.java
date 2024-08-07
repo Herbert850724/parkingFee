650995e67b61d385e790c86145c4e81c8905b153
herbe <herbert850724@gmail.com>
2024-08-07 17:26:19 +0800

    priceBookRepository

--- src/main/java/com/tdd/ParkingFeeCalculator.java
+++ src/main/java/com/tdd/ParkingFeeCalculator.java
@@ -10,7 +10,9 @@ public class ParkingFeeCalculator {
     private Duration FIFTY_MINUTES = Duration.ofMinutes(15L);
 
     public ParkingFeeCalculator() {
-        priceBook = new PriceBook();
+
+        PriceBookRepository priceBookRepository = new PriceBookRepository(new PriceBook());
+        priceBook = priceBookRepository.getPriceBook();
     }
 
     public Long calculate(ParkingSession parkingSession) {
@@ -20,18 +22,10 @@ public class ParkingFeeCalculator {
         if (isShort(duration)) {
             return 0L;
         }
-//        Long totalFee = 0L;
-//
           List<DailySession> dailySessions = parkingSession.getDailySessions();
-//        for(DailySession dailySession : dailySessions){
-//            totalFee += holidayBook.getDailyFee(dailySession);
-//        }
-//        return totalFee;
 
         return dailySessions.stream().mapToLong(priceBook::getDailyFee).sum();
     }
-
-
 
     private boolean isShort(Duration duration){
 
---
+++ src/main/java/com/tdd/PriceBookRepository.java
@@ -0,0 +1,14 @@
+package com.tdd;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PriceBookRepository {
+
+    private PriceBook priceBook;
+
+}
